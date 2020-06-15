5d87529957244208a5e1ac9873fed64b669fa92d
zuberol <zuber.kuba@gmail.com>
2020-06-15 20:29:15 +0200

    playlist added

--- scripts.js
+++ scripts.js
@@ -1,19 +1,22 @@
-var video = document.getElementById('video');
+let video = document.getElementById('video');
  // var videoSrc = 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8';
- // var videoSrc = 'https://test-streams.mux.dev/x36xhzz/url_6/193039199_mp4_h264_aac_hq_7.m3u8';
+  var videoSrc = 'https://test-streams.mux.dev/x36xhzz/url_6/193039199_mp4_h264_aac_hq_7.m3u8';
 
  const movies = [
     'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8',
-    'https://test-streams.mux.dev/x36xhzz/url_6/193039199_mp4_h264_aac_hq_7.m3u8'
+    'https://test-streams.mux.dev/pts_shift/master.m3u8',
+    'https://test-streams.mux.dev/x36xhzz/url_6/193039199_mp4_h264_aac_hq_7.m3u8',
+    'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s-fmp4/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8'
  ]
-
- var videoSrc = 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s-fmp4/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8';
+let hls;
+ //var videoSrc = 'https://bitdash-a.akamaihd.net/content/MI201109210084_1/m3u8s-fmp4/f08e80da-bf1d-4e3d-8899-f0f6155f6efa.m3u8';
 if (Hls.isSupported()) {
-    var hls = new Hls();
+    hls = new Hls();
     hls.loadSource(videoSrc);
     hls.attachMedia(video);
     hls.on(Hls.Events.MANIFEST_PARSED, function() {
-         updateInterface(hls.levels);
+        updateInterface(hls.levels);
+        console.log('1')
         video.play();
     });
 
@@ -21,6 +24,8 @@ if (Hls.isSupported()) {
     hls.on(Hls.Events.LEVEL_SWITCHING, function (event, data) {
         updateInfo(data);
     });
+    
+
 }
 // else if (video.canPlayType('application/vnd.apple.mpegurl')) {
 //     video.src = videoSrc;
@@ -29,8 +34,117 @@ if (Hls.isSupported()) {
 //     });
 // }
 
-function changeClip(){
-    console.log('clicked');
+function changeClip(e){
+    console.log(e.target.innerHTML);
+
+    hls = new Hls();
+
+    hls.loadSource(e.target.innerHTML);
+    hls.attachMedia(video);
+    
+    hls.on(Hls.Events.MANIFEST_PARSED, function() {
+        updateInterface(hls.levels);
+        video.play();
+        console.log('2')
+    });
+
+
+    hls.on(Hls.Events.LEVEL_SWITCHING, function (event, data) {
+        updateInfo(data);
+    });
+
+    // hls.on(Hls.Events.ERROR, function (name, data) {
+    //   console.warn('Error event:', data);
+  
+    //   switch (data.details) {
+    //     case Hls.ErrorDetails.MANIFEST_LOAD_ERROR:
+    //       try {
+    //         $('#errorOut').html('Cannot load <a href="' + data.context.url + '">' + url + '</a><br>HTTP response code:' + data.response.code + ' <br>' + data.response.text);
+  
+    //         if (data.response.code === 0) {
+    //           $('#errorOut').append('This might be a CORS issue, consider installing <a href="https://chrome.google.com/webstore/detail/allow-control-allow-origi/nlfbmbojpeacfghkpbjhddihlkkiljbi">Allow-Control-Allow-Origin</a> Chrome Extension');
+    //         }
+    //       } catch (err) {
+    //         $('#errorOut').html('Cannot load <a href="' + data.context.url + '">' + url + '</a><br>Response body: ' + data.response.text);
+    //       }
+  
+    //       break;
+  
+    //     case Hls.ErrorDetails.MANIFEST_LOAD_TIMEOUT:
+    //       logError('Timeout while loading manifest');
+    //       break;
+  
+    //     case Hls.ErrorDetails.MANIFEST_PARSING_ERROR:
+    //       logError('Error while parsing manifest:' + data.reason);
+    //       break;
+  
+    //     case Hls.ErrorDetails.LEVEL_EMPTY_ERROR:
+    //       logError('Loaded level contains no fragments ' + data.level + ' ' + data.url);
+    //       handleLevelError(data);
+    //       break;
+  
+    //     case Hls.ErrorDetails.LEVEL_LOAD_ERROR:
+    //       logError('Error while loading level playlist ' + data.context.level + ' ' + data.url);
+    //       handleLevelError(data);
+    //       break;
+  
+    //     case Hls.ErrorDetails.LEVEL_LOAD_TIMEOUT:
+    //       logError('Timeout while loading level playlist ' + data.context.level + ' ' + data.url);
+    //       handleLevelError(data);
+    //       break;
+  
+    //     case Hls.ErrorDetails.LEVEL_SWITCH_ERROR:
+    //       logError('Error while trying to switch to level ' + data.level);
+    //       break;
+  
+    //     case Hls.ErrorDetails.FRAG_LOAD_ERROR:
+    //       logError('Error while loading fragment ' + data.frag.url);
+    //       break;
+  
+    //     case Hls.ErrorDetails.FRAG_LOAD_TIMEOUT:
+    //       logError('Timeout while loading fragment ' + data.frag.url);
+    //       break;
+  
+    //     case Hls.ErrorDetails.FRAG_LOOP_LOADING_ERROR:
+    //       logError('Fragment-loop loading error');
+    //       break;
+  
+    //     case Hls.ErrorDetails.FRAG_DECRYPT_ERROR:
+    //       logError('Decrypting error:' + data.reason);
+    //       break;
+  
+    //     case Hls.ErrorDetails.FRAG_PARSING_ERROR:
+    //       logError('Parsing error:' + data.reason);
+    //       break;
+  
+    //     case Hls.ErrorDetails.KEY_LOAD_ERROR:
+    //       logError('Error while loading key ' + data.frag.decryptdata.uri);
+    //       break;
+  
+    //     case Hls.ErrorDetails.KEY_LOAD_TIMEOUT:
+    //       logError('Timeout while loading key ' + data.frag.decryptdata.uri);
+    //       break;
+  
+    //     case Hls.ErrorDetails.BUFFER_APPEND_ERROR:
+    //       logError('Buffer append error');
+    //       break;
+  
+    //     case Hls.ErrorDetails.BUFFER_ADD_CODEC_ERROR:
+    //       logError('Buffer add codec error for ' + data.mimeType + ':' + data.err.message);
+    //       break;
+  
+    //     case Hls.ErrorDetails.BUFFER_APPENDING_ERROR:
+    //       logError('Buffer appending error');
+    //       break;
+  
+    //     case Hls.ErrorDetails.BUFFER_STALLED_ERROR:
+    //       logError('Buffer stalled error');
+    //       break;
+  
+    //     default:
+    //       break;
+    //     }
+    // });
 }
 
 function updatePlaylist(){
@@ -58,7 +172,7 @@ function updateInterface(levels){
     // create buttons
     for(let i=0; i<levels.length; ++i){
         let b = document.createElement("button");
-        console.log(levels[i]);
+        //console.log(levels[i]);
         b.innerHTML = levels[i].height + 'p';
 
         b.classList.add("q-button");
